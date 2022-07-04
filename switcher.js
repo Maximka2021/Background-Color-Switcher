52efbf61028cb70a34c66cdcaa2dec43186cc1ef
Maximka2021 <polyakovmaxim05@gmail.com>
2022-07-04 11:38:14 -0400

    Switcher

---
+++ switcher.js
@@ -0,0 +1,32 @@
+let title = document.querySelector('.title');
+let text = document.querySelector('.text')
+
+function grey(){
+    document.body.style.backgroundColor = 'grey';
+    title.style.color = 'black';
+    text.style.color = 'black';
+}
+
+function white(){
+    document.body.style.backgroundColor = 'white';
+    title.style.color = 'black';
+    text.style.color = 'black';
+}
+
+function blue(){
+    document.body.style.backgroundColor = 'blue';
+    title.style.color = 'black';
+    text.style.color = 'black';
+}
+
+function yellow(){
+    document.body.style.backgroundColor = 'yellow';
+    title.style.color = 'black';
+    text.style.color = 'black';
+}
+
+function black(){
+    document.body.style.backgroundColor = 'black';
+    title.style.color = 'white';
+    text.style.color = 'white'; 
+}
